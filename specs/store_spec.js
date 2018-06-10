a16c2baac7395a0ae450a13e0498399bf60a2128
Stuart O'Donnell <stuartodonnell@gmail.com>
2018-06-10 16:53:50 +0100

    adds passing test for returning a store balance

--- specs/store_spec.js
+++ specs/store_spec.js
@@ -7,7 +7,7 @@ let store;
 let record;
 
 beforeEach(function(){
-    store = new Store("World Records", "Kalamazoo")
+    store = new Store("World Records", "Kalamazoo", 100)
     record1 = new Record("Fugees", "The Score", "Hip-Hop", 10);
     record2 = new Record("Cypress Hill", "Black Sunday", "Hip-Hop", 13);
     record3 = new Record("Prodigy", "Music For A Jilted Generation", "Dance", 9);
@@ -29,6 +29,10 @@ assert.strictEqual(store.inventory.length, 0);
 it("should add a record to a store inventory", function(){
   store.addRecordToInventory(record1);
   assert.strictEqual(store.inventory.length, 1);
+})
+
+it("should have a balance", function(){
+  assert.strictEqual(store.balance, 100);
 })
 
 })
