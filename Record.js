4319a1c4eeb919e7a30193d7a02d3dff14d80e2c
Stuart O'Donnell <stuartodonnell@gmail.com>
2018-06-10 16:21:05 +0100

    adds test for returning record name

--- Record.js
+++ Record.js
@@ -1,8 +1,8 @@
-var Record = function() {
+const Record = function(artist, title, genre, price){
   this.artist = artist;
   this.title = title;
   this.genre = genre;
   this.price = price;
-}
+};
 
 module.exports = Record;
--- specs/record_spec.js
+++ specs/record_spec.js
@@ -1,2 +1,42 @@
 var assert = require('assert')
-var arrayTasks = require('./Record.js')
+var Record = require('../Record.js')
+
+
+
+describe('Array tasks', function () {
+
+let record;
+
+beforeEach(function(){
+    record1 = new Record("Fugees", "The Score", "Hip-Hop", 10);
+    record2 = new Record("Cypress Hill", "Black Sunday", "Hip-Hop", 13);
+    record3 = new Record("Prodigy", "Music For A Jilted Generation", "Dance", 9);
+    record4 = new Record("Beck", "Odelay", "Alternative", 15);
+  })
+
+it("should have an artist", function(){
+record1 = new Record("Fugees", "The Score", "Hip-Hop", 10)
+assert.strictEqual(record1.artist, "Fugees");
+});
+
+xit("should have a title", function(){
+
+})
+xit("should have a genre", function(){
+
+})
+xit("should have a price", function(){
+
+})
+
+
+
+
+
+
+
+
+
+
+
+})
